20e6ae08ec61b783884b1160af63f70d9b0fa5ee
juanzhang <juanzhang@sse.int>
2020-01-08 10:43:33 +0800

    <集成logback>

--- dao/src/main/java/com/dao/UserDao.java
+++ dao/src/main/java/com/dao/UserDao.java
@@ -1,11 +1,16 @@
 package com.dao;
 
 import com.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
+
+    private final static Logger log = LoggerFactory.getLogger(UserDao.class);
+
     @Autowired
     private User user;
 
@@ -14,6 +19,13 @@ public class UserDao {
     }
 
     public String showDao(){
+        log.trace("trace级别的日志");
+        log.debug("debug级别日志");
+        log.info("info级别日志");
+        log.warn("warn级别的日志");
+        log.error("error级别日志");
+        String s = null;
+        System.out.println("s :     "+s.length());
         return user.showEntity()+"    我是Dao！";
     }
 }
